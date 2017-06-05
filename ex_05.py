6cccff74595443463f591bff8dabcfd77368156b
Aurelien Castel <aureliencastel.all@gmail.com>
2017-06-05 21:40:23 +0200

    Add new chapter

--- ex_05.py
+++ ex_05.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-biggest_country = "Russia"
-smallest_country = "Vatican City"
-most_populated_country = "China"
-second_most_populated_country = "India"
+biggest_country = "Russie"
+smallest_country = "Vatican"
+most_populated_country = "Chine"
+second_most_populated_country = "Inde"
 
-print "The biggest country in the world is", biggest_country + "."
-print "The smallest country in the world is", smallest_country + "."
-print "The most populated country in the world is", most_populated_country + "."
-print "The second most populated country in the world is", second_most_populated_country + "."
+print "Le plus grand pays du monde est", biggest_country + "."
+print "Le plus petit pays du monde est", smallest_country + "."
+print "Le pays le plus peuplé du monde est", most_populated_country + "."
+print "Le deuxième pays le plus peuplé du monde est", second_most_populated_country + "."
