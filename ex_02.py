f1fa2451cea83a45074471dbf3825034b930d2c0
Aurelien Castel <aureliencastel.all@gmail.com>
2017-06-12 21:30:56 +0200

    Remove files with old names

--- ex_02.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-print "Bonjour je m'appelle Miguel Sanchez."
-print "La racine carée de 9 est 3."
-print "99.99 pourcent des gens aiment le chocolat."
--- ex_04.py
+++
@@ -1,6 +0,0 @@
-#!usr/bin/env python
-# -*- coding: utf-8 -*-
-
-integer = 13
-floating = 13.0
-string = "13"
